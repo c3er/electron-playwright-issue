143f260235d2eb433b9ebef3073ab8d8f1d8cb2f
Christian Dreier <flipper.x3r@googlemail.com>
2024-05-12 15:15:04 +0200

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# electron-playwright-issue
+
+A minimal Electron application to show that Playwright 1.44 doesn't work with Electron 30.
---
+++ app/main.js
@@ -0,0 +1,30 @@
+const path = require("path")
+
+const electron = require("electron")
+
+function createWindow() {
+    const mainWindow = new electron.BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+        },
+    })
+    mainWindow.loadFile(path.join(__dirname, "index.html"))
+}
+
+electron.app.whenReady().then(() => {
+    createWindow()
+
+    electron.app.on("activate", () => {
+        if (electron.BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+electron.app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        electron.app.quit()
+    }
+})
